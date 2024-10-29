00c4e92ceb0e347591affe81e560aff01c426760
Manojtewatia137@gmail.com <lakhans8833@gmail.com>
2024-10-29 11:04:42 +0530

    first commit

--- js/slider/main.js
+++ js/slider/main.js
@@ -1,18 +1,18 @@
 /*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
+var swiperCards1 = new Swiper(".slider1", {
   loop: true,
   spaceBetween: 32,
   grabCursor: true,
 
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination1",
     clickable: true,
     dynamicBullets: true,
   },
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
   },
 
   breakpoints:{
@@ -23,4 +23,66 @@ let swiperCards = new Swiper(".card__content", {
       slidesPerView: 3,
     },
   },
+
+});
+
+
+
+/*=============== SWIPER JS ===============*/
+var swiperCards2 = new Swiper(".slider2", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+
+  breakpoints:{
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+
+});
+
+
+
+
+/*=============== SWIPER JS ===============*/
+var swiperCards3 = new Swiper(".slider3", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination3",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next3",
+    prevEl: ".swiper-button-prev3",
+  },
+
+  breakpoints:{
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+
 });
